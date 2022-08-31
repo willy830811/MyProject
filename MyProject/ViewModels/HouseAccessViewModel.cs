22f20b265686170a20a917884535dbfd53c4576e
Wei <willy830811@gmail.com>
2022-08-31 15:34:14 +0800

    access 補commit

---
+++ MyProject/ViewModels/HouseAccessViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyProject.Models;
+
+namespace MyProject.ViewModels
+{
+    public class HouseAccessViewModel
+    {
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+        public bool Checked { get; set; }
+    }
+}
